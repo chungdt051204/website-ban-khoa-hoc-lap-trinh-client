6249f47e7e9585b9dac3296225d99999cb36d04c
chungdt051204 <email_github_ca_bn@gmail.com>
2026-01-03 11:51:40 +0700

    second commit

--- src/assets/components/HomeUser.jsx
+++ src/assets/components/HomeUser.jsx
@@ -7,6 +7,7 @@ import CoursesPre from "./CoursesPre";
 export default function HomeUser() {
   return (
     <>
+      <h1>Hello</h1>
       <UserNavBar />
       <Carousel />
       <CoursesFree />
